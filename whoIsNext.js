182e2cfac0f5a184a4175daa12f68b21c81b6fb1
willybob7 <34104463+willybob7@users.noreply.github.com>
2018-10-15 15:55:29 -0400

    Update whoIsNext.js

--- whoIsNext.js
+++ whoIsNext.js
@@ -1,5 +1,4 @@
 function whoIsNext(names, r) {
-  var num = r
   var base = names.length;
   if (r <= base) {
     return names[r - 1];
@@ -11,18 +10,19 @@ function whoIsNext(names, r) {
     trackerArr.push(1);
   }
   var j = 0;
-  var sum = 0;
+  var sum = trackerArr.reduce((a,b)=>a+b);
   i = 0;
-  while (sum <= r) {
+  while (sum < r) {
     var current = trackerArr[j];
-    trackerArr.push(current + current);
+    var add = current + current;
+    trackerArr.push(add);
     string[i]++;
     j++;
     i++;
     if (i == base) {
       i = 0;
     }
-    sum = trackerArr.reduce((a,b)=>a + b);
+    sum += add;
   }
   var a = Math.max(...string);
   var b = string.lastIndexOf(a);
